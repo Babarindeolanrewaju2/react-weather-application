630af123cff0e9bcb8aea84a9a5e12b19f49323b
pie7 <j3pxy6@gmail.com>
2020-03-22 16:38:35 +0800

    Refactor and Add fetchForecast5day3hour, fetchCurrentWeather saga functions

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,28 +1,44 @@
 
-import { take, put, call, fork, select } from 'redux-saga/effects'
-import * as actions from '../actions'
-
-import { loc } from '../reducers/fetch'
-
+import { take, put, call, fork, all } from 'redux-saga/effects'
 import APIUtilits from '../api/APIUtils'
+import * as ACTIONS from '../actions'
 
-const APIKEY = process.env.REACT_APP_LOCATION_APIKEY
+const api = (url) => fetch(url).then(response => response.json())
 
-function* fetchloc(action) {
+function* fetchLocation(action) {
   try {
-    let loc = yield fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${APIKEY}`);
-    loc = yield loc.json();
-    console.log(loc);
-
-    yield put({ type: 'FETCH_LOC', loc });
+    const ipLocation = yield call(api, APIUtilits.IPGeoLocation())
+    yield put({ type: ACTIONS.FETCH_LOCACTION_SUCCESS, data: ipLocation })
+    yield call(fetchCurrentWeather, ipLocation.latitude, ipLocation.longitude)
+    yield call(fetchForecast5day3hour, ipLocation.latitude, ipLocation.longitude)
   } catch (error) {
-    yield put({ type: 'FETCH_LOC_ERROR', error });
+    yield put({ type: ACTIONS.FETCH_ERROR, error });
+  }
+}
+
+
+function* fetchForecast5day3hour(latitude = 35.6586, longitude = 139.7454) {
+  try {
+    const forecast5day3hour = yield call(api, APIUtilits.forecast5d3h(latitude, longitude))
+    yield put({ type: ACTIONS.FETCH_5DAY3HOUR_SUCCESS, data: forecast5day3hour })
+  } catch (error) {
+    yield put({ type: ACTIONS.FETCH_ERROR, error });
+  }
+}
+
+
+function* fetchCurrentWeather(latitude = 35.6586, longitude = 139.7454) {
+  try {
+    const currentWeather = yield call(api, APIUtilits.currentWeather(latitude, longitude))
+    yield put({ type: ACTIONS.FETCH_CURRENT_WEATHER_SUCCESS, data: currentWeather })
+  } catch (error) {
+    yield put({ type: ACTIONS.FETCH_ERROR, error });
   }
 }
 
 export function* startup() {
-   yield fork(fetchloc)
- }
+  yield call(fetchLocation)
+}
 
  export default function* root() {
    yield fork(startup)
