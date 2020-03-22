134c66329e00014ca876443422d00416c4a8aad0
pie7 <j3pxy6@gmail.com>
2020-03-22 16:34:16 +0800

    Refactor App component to funcion component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,69 +1,44 @@
 import * as React from 'react'
-
 import { connect } from 'react-redux'
-
 import Location from '../components/Location'
 import CurrentDate from '../components/CurrentDate'
 import Tempertaure from '../components/Temperature'
 import Forecast from '../components/Forecast3hour'
-import APIUtilits from '../api/APIUtils'
 import './index.scss'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      currentWeather: {},
-      ipLocation: {},
-      forecast5d3h: {}
-    }
-  }
+function App({ currentWeather, ipLocation, forecast5d3h, isFetching }) {
 
-  componentDidMount() {
-    APIUtilits.getIPGeoLocation()
-      .then(res => {
-        Promise.all([
-          APIUtilits.getIPGeoLocation(),
-          APIUtilits.getForecast5d3h(res.latitude, res.longitude),
-          APIUtilits.getCurrentWeather(res.latitude, res.longitude)
-        ])
-          .then(res => {
-            this.setState({
-              ipLocation: res[0],
-              forecast5d3h: res[1],
-              currentWeather: res[2]
-            })
-          })
-      })
-  }
+  const { city, country_name } = ipLocation || {}
+  const { list } = forecast5d3h || {}
+  const { main: { temp } = {}, weather, dt } = currentWeather || {}
 
-  render() {
-    const { currentWeather, ipLocation, forecast5d3h } = this.state
-
-    const { city, country_name } = ipLocation
-    const { list } = forecast5d3h
-    const { main: { temp } = {}, weather, dt } = currentWeather
-
-    return (
-      <div className="app__container">
-        <Location
-          city={city}
-          countryName={country_name}
-        />
-        <CurrentDate
-          currentTime={dt}
-        />
-        <Tempertaure
-          temperature={temp && temp}
-          icon={weather && weather[0].icon}
-          description={weather && weather[0].description}
-        />
-        <Forecast
-          fiveForecast={list && list.slice(0, 5)}
-        />
+  return isFetching ? (
+    <div className="app__container">
+      <Location
+        city={city}
+        countryName={country_name}
+      />
+      <CurrentDate
+        currentTime={dt}
+      />
+      <Tempertaure
+        temperature={temp && temp}
+        icon={weather && weather[0].icon}
+        description={weather && weather[0].description}
+      />
+      <Forecast
+        fiveForecast={list && list.slice(0, 5)}
+      />
+    </div>
+  ) :
+    <div className="app__load">
+      <div className="load__wrap">
+        <p className="load__text">Loading...</p>
+        <div className="load__dot"></div>
+        <div className="load__dot"></div>
+        <div className="load__dot"></div>
       </div>
-    )
-  }
+    </div>
 }
 
 function mapStateToProps(state) {
